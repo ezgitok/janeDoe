e9078a473fdc55db6a01b751c5366c7602f60080
ezgitok <etok4127@gmail.com>
2017-08-20 09:28:26 +0300

    pls!

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-console.log('Hello world!!!')
 const express = require('express');
+
 const expressApplication = express();
+
 expressApplication.get('/', (request, response) => {
   response.send('Hello world!');
 });
+
 expressApplication.listen(1337, () => {
   console.log('Application is now listening to HTTP requests...');
   console.log('Press CTRL+C to stop.');
